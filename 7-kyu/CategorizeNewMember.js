274dab01d28a143ea3954f75265506e31853a5d6
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-13 11:41:06 +0200

    solve kata

--- 7-kyu/CategorizeNewMember.js
+++ 7-kyu/CategorizeNewMember.js
@@ -1,17 +1,18 @@
 function openOrSenior(data){
-  
+  let result = []
+  data.forEach((person) => {
+    age = person[0]
+    handi = person[1]
+    if (age >= 55 && handi > 7) {
+      result.push('Senior');
+    } else {
+      result.push('Open');
+    }
+  });
+  return result 
 }
 
-arr = [[45, 12], [55, 21], [19, -2], [104, 20]]
-let age = 0
-let handi = 0 
-arr.forEach((person) => {
-  age = person[0]
-  handi = person[1]
-  console.log(age, handi)
-  if (age >= 55 && handi > 7) {
-    
-  }
-});
 
-// console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]));
+
+// arr = [[45, 12], [55, 21], [19, -2], [104, 20]]
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]));
