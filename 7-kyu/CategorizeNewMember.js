7b07f5b1b1d55c9ad9767f145b9890e240f388d0
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-13 10:32:18 +0200

    initial trial

---
+++ 7-kyu/CategorizeNewMember.js
@@ -0,0 +1,17 @@
+function openOrSenior(data){
+  
+}
+
+arr = [[45, 12], [55, 21], [19, -2], [104, 20]]
+let age = 0
+let handi = 0 
+arr.forEach((person) => {
+  age = person[0]
+  handi = person[1]
+  console.log(age, handi)
+  if (age >= 55 && handi > 7) {
+    
+  }
+});
+
+// console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]));
