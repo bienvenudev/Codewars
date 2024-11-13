a9819a28f48a7b4482bb25869914d9bf4cc62490
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-13 11:45:46 +0200

    change formatting

--- 7-kyu/CategorizeNewMember.js
+++ 7-kyu/CategorizeNewMember.js
@@ -1,18 +1,23 @@
-function openOrSenior(data){
-  let result = []
+function openOrSenior(data) {
+  let result = [];
   data.forEach((person) => {
-    age = person[0]
-    handi = person[1]
+    age = person[0];
+    handi = person[1];
     if (age >= 55 && handi > 7) {
-      result.push('Senior');
+      result.push("Senior");
     } else {
-      result.push('Open');
+      result.push("Open");
     }
   });
-  return result 
+  return result;
 }
 
-
-
 // test should output = [ 'Open', 'Senior', 'Open', 'Senior' ]
-console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]));
+console.log(
+  openOrSenior([
+    [45, 12],
+    [55, 21],
+    [19, -2],
+    [104, 20],
+  ])
+);
