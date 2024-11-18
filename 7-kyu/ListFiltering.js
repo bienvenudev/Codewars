f6c01d51c2cae373f0962e663a6ae4cd0c652146
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-18 11:17:19 +0200

    add 2nd approach using filter

--- 7-kyu/ListFiltering.js
+++ 7-kyu/ListFiltering.js
@@ -2,14 +2,20 @@
 // create new array
 // map through if number add to new array else do nothing
 
+// function filter_list(l) {
+//   const result = []
+//   l.map((item) => {
+//     if (typeof (item) === 'number') {
+//       result.push(item)
+//     }
+//   })
+//   return result
+// }
+
+// 2nd approach using filter
 function filter_list(l) {
-  const result = [];
-  l.map((item) => {
-    if (typeof item === "number") {
-      result.push(item);
-    }
-  });
-  return result;
+  const filtered = l.filter((item) => typeof item === "number");
+  return filtered;
 }
 
 // should return [1,2]
