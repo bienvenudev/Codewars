53b6c2bed88940d695a9b17b9fd1122f6fde97b8
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-11-18 11:09:33 +0200

    initial approach using map to filter

---
+++ 7-kyu/ListFiltering.js
@@ -0,0 +1,16 @@
+// 1st approach using map
+// create new array
+// map through if number add to new array else do nothing
+
+function filter_list(l) {
+  const result = [];
+  l.map((item) => {
+    if (typeof item === "number") {
+      result.push(item);
+    }
+  });
+  return result;
+}
+
+// should return [1,2]
+console.log(filter_list([1, 2, "a", "b"]));
