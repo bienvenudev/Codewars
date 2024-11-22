6e27021e3250ca51986583aacf1c85eaf8e657d4
bienvenudev <cbienvenu007@gmail.com>
2024-11-22 15:29:30 +0200

    better approach using str.repeat

--- 8-kyu/stringy.js
+++ 8-kyu/stringy.js
@@ -1,16 +1,26 @@
 function stringy(size) {
-  // loop using size
-  const arr = [];
-  for (i = 0; i < size; i++) {
-    arr.push(1);
-    arr.push(0);
-  }
-  
-  const newarr = []
-  for (i = 0; i < size; i++) {
-    newarr.push(arr[i]);
-  }
-    return newarr.join("")
+  // better approach using str.repeat
+  let string = ''
+  string += '10'.repeat(size).slice(0, size)
+  return string
 }
+
+// output should be 1010
+console.log(stringy(4))
+
+// function stringy(size) {
+//   // loop using size
+//   const arr = [];
+//   for (i = 0; i < size; i++) {
+//     arr.push(1);
+//     arr.push(0);
+//   }
   
-console.log(stringy(5))
+//   const newarr = []
+//   for (i = 0; i < size; i++) {
+//     newarr.push(arr[i]);
+//   }
+//     return newarr.join("")
+// }
+  
+// console.log(stringy(5))
