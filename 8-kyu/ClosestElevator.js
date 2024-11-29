c42f5e19f44f84691fe23330a6055de40ed978c4
bienvenudev <cbienvenu007@gmail.com>
2024-11-29 10:12:21 +0200

    use clean approach using Math.abs()

--- 8-kyu/ClosestElevator.js
+++ 8-kyu/ClosestElevator.js
@@ -1,31 +1,13 @@
-function elevator(left, right, call){
-  // call should be close to left or right/ 
-  // interval btn call and left or right should be close 
-  
-  const leftDist = call - left 
-  const rightDist = call - right 
-  
-if (leftDist < 0 && rightDist < 0) {
-    if (rightDist > leftDist) {
-    //   console.log('oops')
-    return 'right'
-  } else if (leftDist > rightDist) {
-    return 'left'
-  } else if (leftDist === rightDist) {
-        return 'right'
-  }     
-    } else if (leftDist === rightDist) {
-        return 'right'
-  } else if (call === right && call === left) {
-    return 'right'
-  } else if (call === right) {
-    return 'right'
-  } else if (call === left) {
-    return 'left'
-  } else if (rightDist < leftDist) {
-    //   console.log('oops')
-    return 'right'
-  } else if (leftDist < rightDist) {
-    return 'left'
+function elevator(left, right, call) {
+  // use Math.abs to calculate distance
+  const leftDist = Math.abs(call - left);
+  const rightDist = Math.abs(call - right);
+
+  // return right if the distance is equal
+  if (leftDist === rightDist) {
+    return "right";
   }
+
+  // return the closest elevator
+  return leftDist < rightDist ? "left" : "right";
 }
