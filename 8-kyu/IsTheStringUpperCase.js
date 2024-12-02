78c7016d85580885e684b541bc0d9adc9d86f0ed
bienvenudev <cbienvenu007@gmail.com>
2024-12-02 10:18:31 +0200

    use ternary operator to shorten the code

--- 8-kyu/IsTheStringUpperCase.js
+++ 8-kyu/IsTheStringUpperCase.js
@@ -5,11 +5,7 @@ String.prototype.isUpperCase = function () {
 
   // if letter is not uppercase push false to results else push true
   array.forEach((letter) => {
-    if (letter.toUpperCase() !== letter) {
-      results.push(false);
-    } else {
-      results.push(true);
-    }
+    letter.toUpperCase() !== letter ? results.push(false) : results.push(true)
   });
 
   // if results array includes false immediately return false (found a lowercase letter)
