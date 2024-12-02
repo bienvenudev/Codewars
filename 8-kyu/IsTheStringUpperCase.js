80a4afddd4cec66d0558b48a4c316a2f6201e893
bienvenudev <cbienvenu007@gmail.com>
2024-12-02 10:18:04 +0200

    solve using foreach and includes

---
+++ 8-kyu/IsTheStringUpperCase.js
@@ -0,0 +1,22 @@
+String.prototype.isUpperCase = function () {
+  // split into array
+  const array = this.split("");
+  const results = [];
+
+  // if letter is not uppercase push false to results else push true
+  array.forEach((letter) => {
+    if (letter.toUpperCase() !== letter) {
+      results.push(false);
+    } else {
+      results.push(true);
+    }
+  });
+
+  // if results array includes false immediately return false (found a lowercase letter)
+  return results.includes(false) ? false : true
+};
+
+// test should return false
+console.log("CItY".isUpperCase());
+// test should return true
+console.log("CITY".isUpperCase());
