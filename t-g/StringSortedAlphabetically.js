444a391000c5eb4d922ec18665a9c31e6dab9162
bienvenudev <cbienvenu007@gmail.com>
2024-12-03 16:49:36 +0200

    solve using .includes

--- t-g/StringSortedAlphabetically.js
+++ t-g/StringSortedAlphabetically.js
@@ -13,9 +13,44 @@ a = "xyaabbbccccdefww";
 b = "xxxxyyyyabklmopq";
 
 // using Set
+/* 
 const combinedString = a + b;
 const arrayOfString = combinedString.split("");
 const set = new Set(arrayOfString);
 const arr = [...set];
 
 console.log(arr.sort().join(""));
+*/
+
+a = "xyaabbbccccdefww";
+b = "xxxxyyyyabklmopq";
+
+// using .includes
+const arrayA = a.split("");
+const uniqueA = [];
+
+for (let i = 0; i < arrayA.length; i++) {
+  if (!uniqueA.includes(arrayA[i])) {
+    uniqueA.push(arrayA[i]);
+  }
+}
+
+const arrayB = b.split("");
+const uniqueB = [];
+
+for (let i = 0; i < arrayB.length; i++) {
+  if (!uniqueB.includes(arrayB[i])) {
+    uniqueB.push(arrayB[i]);
+  }
+}
+
+const combinedArray = uniqueA.concat(uniqueB);
+const uniqueArray = [];
+
+for (let i = 0; i < combinedArray.length; i++) {
+  if (!uniqueArray.includes(combinedArray[i])) {
+    uniqueArray.push(combinedArray[i]);
+  }
+}
+
+console.log(uniqueArray.sort().join(''));
