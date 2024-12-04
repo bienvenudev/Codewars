ea2b31b67a809d348e37e359b2ce8d1949f742a0
bienvenudev <cbienvenu007@gmail.com>
2024-12-04 10:09:14 +0200

    start from 0 and check the multiples of x

--- 8-kyu/CountByX.js
+++ 8-kyu/CountByX.js
@@ -1,12 +1,17 @@
 function countBy(x, n) {
+  // create a new empty array
+  // use a for loop and start from 0
+  // if a number is divisible by x add it to the array
+  // stop when new array.length reaches n
   let z = [];
-  let start = x
-  
-  for (let i = x-1; i <= n; i++) {
-    z.push(x * i)
+
+  for (let i = 1; z.length < n; i++) {
+    if (i % x == 0) {
+      z.push(i);
+    }
   }
-  // z.unshift(start)
   return z;
 }
 
-console.log(countBy(2, 5))
+// test should return [ 2, 4, 6, 8, 10 ]
+console.log(countBy(2, 5));
