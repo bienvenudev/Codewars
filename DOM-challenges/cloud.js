a524df6ef2c5801a4f828bf3da16a824f8df9d75
bienvenudev <cbienvenu007@gmail.com>
2024-12-08 19:53:01 +0200

    retrieve most occurences

--- DOM-challenges/cloud.js
+++ DOM-challenges/cloud.js
@@ -1,11 +1,12 @@
 const myParagraph = document.querySelector("#myParagraph").innerText;
+const myWordCloud = document.querySelector("#myWordCloud");
 // replace all symbols with empty
 const cleanParagraph = myParagraph
   .replaceAll(".", "")
   .replaceAll(",", "")
   .replaceAll(":", "")
   .replaceAll(";", "")
-  .replaceAll("\n", "");
+  .replaceAll("\n", " ");
 
 // console.log("Whole para: ", myParagraph);
 
@@ -20,4 +21,34 @@ const cleanParagraph = myParagraph
 // console.log(cleanParagraph.includes("\n"));
 
 const paragraphArray = cleanParagraph.split(" ");
-console.log(paragraphArray);
+const unsortedObject = {};
+
+for (let i = 0; i < paragraphArray.length; i++) {
+  const currentWord = paragraphArray[i];
+
+  if (currentWord in unsortedObject) {
+    unsortedObject[currentWord]++;
+  } else {
+    unsortedObject[currentWord] = 1;
+  }
+}
+
+// console.log(unsortedObject);
+
+const occurrenceArray = [];
+
+// move properties and values to an array
+for (let word in unsortedObject) {
+  occurrenceArray.push([word, unsortedObject[word]]);
+}
+
+// sort the array
+const sortedArray = occurrenceArray.sort((a, b) => b[1] - a[1]);
+// extract 12 most occurrences
+const mostOccurrencesArray = sortedArray.slice(0, 12);
+// retrieve 12 most occurring words
+const mostOccurrences = mostOccurrencesArray.map((item) => item[0]);
+
+console.log(mostOccurrences);
+
+// myWordCloud.textContent = mostOccurrences;
