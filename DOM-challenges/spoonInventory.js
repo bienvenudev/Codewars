f56f0ce4784654cdea17a4df06a85f5ebd82c45e
bienvenudev <cbienvenu007@gmail.com>
2024-12-08 17:03:18 +0200

    add the spoon inventory challenge

---
+++ DOM-challenges/spoonInventory.js
@@ -0,0 +1,29 @@
+/**
+ * The current spoons the Spoon Central CEO has in the
+ * Spoon Central warehouse. And by warehouse I mean
+ * the spoons they have at home.
+ */
+
+const spoonInventory = [
+  "Tiny Spoon",
+  "Spoon of Destiny",
+  "Rusty Spoon",
+  "Mega Spoon 5000",
+  "Mega Spoon 5000",
+  "Plastic Spoon",
+  "Wooden Spoon",
+  "Rusty Spoon",
+  "Mega Spoon 5000",
+  "Ultra Spoon Pro Max 11",
+  "Tiny Spoon",
+  "Rusty Spoon",
+  "Mega Spoon 5000",
+  "Mega Spoon 5000",
+  "Plastic Spoon",
+  "Ultra Spoon Pro Max 11",
+];
+
+/** A function for counting spoons. Wow. */
+function countSpoons() {
+  // Your code here
+}
