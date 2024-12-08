65bb23cd7cb99e7a7046eae125a16b084e1625fa
bienvenudev <cbienvenu007@gmail.com>
2024-12-08 20:37:17 +0200

    complete word cloud with decremented font size

--- DOM-challenges/cloud.js
+++ DOM-challenges/cloud.js
@@ -1,5 +1,6 @@
 const myParagraph = document.querySelector("#myParagraph").innerText;
 const myWordCloud = document.querySelector("#myWordCloud");
+
 // replace all symbols with empty
 const cleanParagraph = myParagraph
   .replaceAll(".", "")
@@ -8,32 +9,20 @@ const cleanParagraph = myParagraph
   .replaceAll(";", "")
   .replaceAll("\n", " ");
 
-// console.log("Whole para: ", myParagraph);
-
-// console.log("-----");
-// console.log("-----");
-// console.log("-----");
-
-// console.log("Clean para: ", cleanParagraph);
-
-// split into array
-
-// console.log(cleanParagraph.includes("\n"));
-
+// split paragraph into an array
 const paragraphArray = cleanParagraph.split(" ");
 const unsortedObject = {};
 
 for (let i = 0; i < paragraphArray.length; i++) {
   const currentWord = paragraphArray[i];
 
+  // check if the current word is already in the object
   if (currentWord in unsortedObject) {
-    unsortedObject[currentWord]++;
+    unsortedObject[currentWord]++; // increment count if it exists
   } else {
-    unsortedObject[currentWord] = 1;
+    unsortedObject[currentWord] = 1; // initialize count to 1 if it doesn't exist
   }
 }
-
-// console.log(unsortedObject);
 
 const occurrenceArray = [];
 
@@ -44,14 +33,17 @@ for (let word in unsortedObject) {
 
 // sort the array
 const sortedArray = occurrenceArray.sort((a, b) => b[1] - a[1]);
-// extract 12 most occurrences
+// extract 12 most occurrences(words and occurrence count)
 const mostOccurrencesArray = sortedArray.slice(0, 12);
-// retrieve 12 most occurring words
+// retrieve 12 most occurring words (words only)
 const mostOccurrences = mostOccurrencesArray.map((item) => item[0]);
 
-console.log(mostOccurrences);
-
-mostOccurrences.forEach((word, i) => {
-  myWordCloud.innerHTML += `<p class='paragraph${i + 1}' >${word}</p>`;
-});
-
+let count = 64;
+for (let i = 0; i < mostOccurrences.length; i++) {
+  const currentWord = mostOccurrences[i];
+  // word cloud with decrement font size (decrement by 4 pixels)
+  myWordCloud.innerHTML += `<p class='paragraph${
+    i + 1
+  }' style="font-size:${count}px">${currentWord}</p>`;
+  count -= 4;
+}
