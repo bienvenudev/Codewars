31d55316f3c4963fd49f6073895af17c9783c78a
bienvenudev <cbienvenu007@gmail.com>
2024-12-08 16:40:00 +0200

    solve using array and join back to string

--- 8-kyu/DrawStairs.js
+++ 8-kyu/DrawStairs.js
@@ -1,12 +1,22 @@
 function drawStairs(n) {
+  // initial space to be incremented
   let space = "";
-  let char = "I\n";
-  let results = [];
+  // I to form steps
+  const step = "I";
+  // array to hold final results
+  const results = [];
   for (i = 0; i < n; i++) {
-    results.push(`${char} ${space}`);
+    // add the last step with no extra space
+    if (i === n - 1) {
+      results.push(step);
+      break;
+    }
+    results.push(`${step}\n ${space}`);
     space += " ";
   }
-  console.log(results.join(''));
+  // join the array back into a string
+  console.log(results.join(""));
 }
 
+// should return exactly 5 steps
 drawStairs(5);
