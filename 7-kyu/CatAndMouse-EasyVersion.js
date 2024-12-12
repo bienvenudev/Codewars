d446d7addb3b1c0a7f222b3bbdc0b53de8584f51
bienvenudev <cbienvenu007@gmail.com>
2024-12-12 11:54:31 +0200

    solve using logical and/or operators

--- 7-kyu/CatAndMouse-EasyVersion.js
+++ 7-kyu/CatAndMouse-EasyVersion.js
@@ -1,8 +1,17 @@
-function catMouse(x){
-
+function catMouse(x) {
+  function catMouse(x){
+    if (x[0] === 'C' && x[1] === 'm' ||
+        x[1] === "." && x[2] === 'm' ||
+        x[1] === '.' && x[2] === '.' && x[3] === "m" ||
+        x[1] === '.' && x[2] === '.' && x[3] === '.' && x[4] === 'm') {
+       return "Caught!";
+     } else {
+       return "Escaped!";
+     }
+   }
 }
 
 // should return "Escaped!"
-console.log(catMouse('C....m'));
+console.log(catMouse("C....m"));
 // should return "Caught!"
-console.log(catMouse('C..m'));
+catMouse("C...m");
