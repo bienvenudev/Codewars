9e1aa4e7c697fd7f751c2800c069660c33e7373b
bienvenudev <cbienvenu007@gmail.com>
2024-12-12 12:31:30 +0200

    solve using for loop, split, and join

--- 6-kyu/BreakCamelCase.js
+++ 6-kyu/BreakCamelCase.js
@@ -1,3 +1,16 @@
 function solution(string) {
-  
+  const splittedString = string.split("");
+  const newArr = [...splittedString];
+
+  for (i = 0; i < splittedString.length; i++) {
+    if (splittedString[i] == splittedString[i].toUpperCase()) {
+      // console.log('found uppercase: ' + splittedString[i])
+      const index = i;
+      newArr.splice(index + (newArr.length - splittedString.length), 0, " ");
+    }
+  }
+  return newArr.join("");
 }
+
+// should return 'hello There Bob'
+solution("helloThereBob");
