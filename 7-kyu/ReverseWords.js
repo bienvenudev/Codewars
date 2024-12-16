d89c214e8a17b652b992a0b3bcb1f388c82d7166
bienvenudev <cbienvenu007@gmail.com>
2024-12-16 10:30:34 +0200

    solve using split, reverse, and join

--- 7-kyu/ReverseWords.js
+++ 7-kyu/ReverseWords.js
@@ -1,3 +1,8 @@
 function reverseWords(str) {
-  // Go for it
+  return str.split("").reverse().join('').split(' ').reverse().join(' ');
 }
+
+// should return 'ehT kciuq nworb xof spmuj revo eht yzal .god'
+console.log(reverseWords("The quick brown fox jumps over the lazy dog."));
+
+
