ffc666fde5c8c74dd60a20353371781674a02060
bienvenudev <cbienvenu007@gmail.com>
2024-12-16 09:55:29 +0200

    add 2nd approach using 2 for loops

--- 7-kyu/VowelCount.js
+++ 7-kyu/VowelCount.js
@@ -15,3 +15,16 @@ function getCount(str) {
   }
   return count;
 }
+
+/* 2nd approach using 2 for loops
+str = 'abracadabra i love you'
+vowels = ['a', 'e', 'i', 'o', 'u']
+vowelsCount = 0;
+
+for(i=0; i<str.length; i++) {
+    for(j=0; j<vowels.length; j++) {
+        if(str[i] == vowels[j]) {
+            vowelsCount++
+        }
+    }
+} */
