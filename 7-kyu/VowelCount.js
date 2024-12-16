f300d27187b968e84c7aac764577f8c1753e78a9
bienvenudev <cbienvenu007@gmail.com>
2024-12-16 09:41:22 +0200

    solve using if statement

--- 7-kyu/VowelCount.js
+++ 7-kyu/VowelCount.js
@@ -1,3 +1,17 @@
 function getCount(str) {
-  return 0;
+  const arr = str.split("");
+  let count = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    if (
+      arr[i] == "a" ||
+      arr[i] == "e" ||
+      arr[i] == "i" ||
+      arr[i] == "o" ||
+      arr[i] == "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
 }
