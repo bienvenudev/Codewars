92b01a9d9c0abee3fcfc42b4f94c773f00107c85
bienvenudev <cbienvenu007@gmail.com>
2024-12-18 10:36:19 +0200

    solve using loop and join

--- 7-kyu/SquareEveryDigit.js
+++ 7-kyu/SquareEveryDigit.js
@@ -1,3 +1,17 @@
-function squareDigits(num){
-  return 0;
+function squareDigits(num) {
+  /// split the num into an array
+  /// loop over the array and square each number
+  /// push the new squares in a new array
+  /// join the array and turn into number again
+
+  const numString = num.toString();
+  const resultArray = [];
+
+  for (let i = 0; i < numString.length; i++) {
+    resultArray.push(numString[i] * numString[i]);
+  }
+  return Number(resultArray.join(""));
 }
+
+// should return 4925
+console.log(squareDigits(235));
