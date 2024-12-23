0f8f6e420ce99a758f1156c92ac42b48baefeadc
bienvenudev <cbienvenu007@gmail.com>
2024-12-23 09:55:50 +0200

    complete challenge with counter and loop

--- 7-kyu/ExesandOhs.js
+++ 7-kyu/ExesandOhs.js
@@ -1,8 +1,24 @@
 function XO(str) {
-  //code here
+  // turn into lowercase
+  // create 2 counters, one for o another for x
+  // loop through and if you find x increment counter same for o
+
+  const lowerString = str.toLowerCase();
+  let countX = 0;
+  let countO = 0;
+
+  for (let i = 0; i < lowerString.length; i++) {
+    const currentString = lowerString[i];
+    if (currentString == "x") {
+      countX++;
+    } else if (currentString == "o") {
+      countO++;
+    }
+  }
+  console.log(countO == countX);
 }
 
-// test
+// tests
 XO("xo"); //true
 XO("xxOo"); //true
 XO("xxxm"); //false
