dd8db830e998bd5a8ace54eaad79d2ff55f9766e
bienvenudev <cbienvenu007@gmail.com>
2024-12-24 11:03:20 +0200

    complete using loop and Math.min

--- 7-kyu/ShortestWord.js
+++ 7-kyu/ShortestWord.js
@@ -3,6 +3,16 @@ function findShort(s) {
   // store shortest length in a new arr
   // use Math.min on the arr
 
+  const arrayOfString = s.split(' ');
+  const allLength = [];
+
+    for (let i = 0; i < arrayOfString.length; i++) {
+      const currentWordLength = arrayOfString[i].length;
+
+      allLength.push(currentWordLength);
+  }
+
+  console.log(Math.min(...allLength));
   
 
 }
