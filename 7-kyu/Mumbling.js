2cc8543f5e36ae45a2742647a1e82784ccca3545
bienvenudev <cbienvenu007@gmail.com>
2024-12-24 12:07:59 +0200

    complete using loop and repeat

--- 7-kyu/Mumbling.js
+++ 7-kyu/Mumbling.js
@@ -1,10 +1,22 @@
 function accum(s) {
-  // your code
+  // turn into uppercase
+  // split into array
+  //
+
+  const upper = s.toUpperCase().split("");
+  const resultArr = []
+  for (let i = 0; i < upper.length; i++) {
+    const currentWord = upper[i];
+    const repeated = currentWord.toLowerCase().repeat(i)
+    resultArr.push(currentWord + repeated);
+  }
+  console.log(resultArr.join('-'));
+  
 }
 
 // tests
 accum("ZpglnRxqenU"); // should return "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
-accum("NyffsGeyylB");
-accum("MjtkuBovqrU");
-accum("EvidjUnokmM");
-accum("HbideVbxncC");
+// accum("NyffsGeyylB");
+// accum("MjtkuBovqrU");
+// accum("EvidjUnokmM");
+// accum("HbideVbxncC");
