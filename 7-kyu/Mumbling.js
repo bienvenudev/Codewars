e0d12fe1cac3a811d45547272eccdd80c4de187e
bienvenudev <cbienvenu007@gmail.com>
2024-12-24 11:25:25 +0200

    start accum challenge

---
+++ 7-kyu/Mumbling.js
@@ -0,0 +1,10 @@
+function accum(s) {
+  // your code
+}
+
+// tests
+accum("ZpglnRxqenU"); // should return "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+accum("NyffsGeyylB");
+accum("MjtkuBovqrU");
+accum("EvidjUnokmM");
+accum("HbideVbxncC");
