b1f78e11dde2267e26bed1eff28418a40ed956cb
bienvenudev <cbienvenu007@gmail.com>
2024-12-24 10:58:53 +0200

    start find short challenge

---
+++ 7-kyu/ShortestWord.js
@@ -0,0 +1,13 @@
+function findShort(s) {
+  // split and loop through arr
+  // store shortest length in a new arr
+  // use Math.min on the arr
+
+  
+
+}
+
+// tests
+findShort("bitcoin take over the world maybe who knows perhaps");
+findShort("turns out random test cases are easier than writing out basic ones");
+findShort("Let's travel abroad shall we");
