df67997a8cd9f3c415179e81f594db518d16da93
bienvenudev <cbienvenu007@gmail.com>
2024-12-24 11:21:32 +0200

    refine with map

--- 7-kyu/ShortestWord.js
+++ 7-kyu/ShortestWord.js
@@ -3,18 +3,9 @@ function findShort(s) {
   // store shortest length in a new arr
   // use Math.min on the arr
 
-  const arrayOfString = s.split(' ');
-  const allLength = [];
-
-    for (let i = 0; i < arrayOfString.length; i++) {
-      const currentWordLength = arrayOfString[i].length;
-
-      allLength.push(currentWordLength);
-  }
-
-  console.log(Math.min(...allLength));
-  
-
+  const arrayOfString = s.split(" ");
+  const shortestLength = Math.min(...arrayOfString.map((x) => x.length));
+  console.log(shortestLength);
 }
 
 // tests
