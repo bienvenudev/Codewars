fe81d04dea5d903b558291e1bb7ffbc904d92a6a
bienvenudev <cbienvenu007@gmail.com>
2024-12-24 12:13:53 +0200

    refine using map and join

--- 7-kyu/Mumbling.js
+++ 7-kyu/Mumbling.js
@@ -1,21 +1,12 @@
 function accum(s) {
-  // turn into uppercase
-  // split into array
-  //
-
-  const upper = s.toUpperCase().split("");
-  const resultArr = []
-  for (let i = 0; i < upper.length; i++) {
-    const currentWord = upper[i];
-    const repeated = currentWord.toLowerCase().repeat(i)
-    resultArr.push(currentWord + repeated);
-  }
-  console.log(resultArr.join('-'));
-  
+  return s
+    .split("")
+    .map((char, i) => char.toUpperCase() + char.toLowerCase().repeat(i))
+    .join("-");
 }
 
 // tests
-accum("ZpglnRxqenU"); // should return "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum("ZpglnRxqenU")); // should return "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
 // accum("NyffsGeyylB");
 // accum("MjtkuBovqrU");
 // accum("EvidjUnokmM");
